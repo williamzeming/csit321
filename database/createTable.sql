bf0be4aae0674d0774fe656d84438ef499a20408
Deltaness <deltaness118@gmail.com>
2022-08-01 21:55:21 +1000

    db add password

--- database/createTable.sql
+++ database/createTable.sql
@@ -1,1 +1,12 @@
-select * from customer;
+create table userinfo
+(
+    userNum   int auto_increment
+        primary key,
+    cusDOB    date        not null,
+    password  varchar(50) not null ,
+    firstName varchar(50) not null,
+    lastName  varchar(50) not null,
+    phoneNum  varchar(50) not null,
+    email     varchar(50) not null,
+    gender    varchar(50) not null
+);
